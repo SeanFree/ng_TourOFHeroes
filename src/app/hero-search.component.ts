daf76c3a96f73e59c888015fd938089f57e2d5ea
Sean Free <seanalexanderfree@gmail.com>
2017-05-20 11:00:53 -0400

    Second Commit

--- src/app/hero-search.component.ts
+++ src/app/hero-search.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
-import 'rxjs/observable/of';
+import 'rxjs/add/observable/of';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
@@ -20,5 +20,33 @@ import { Hero } from './hero';
   providers: [ HeroSearchService ]
 })
 export class HeroSearchComponent implements OnInit {
+  heroes:Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
   
+  constructor(
+    private heroSearchService:HeroSearchService,
+    private router:Router
+  ) { }
+
+  ngOnInit():void {
+    this.heroes = this.searchTerms
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap(term => term
+        ? this.heroSearchService.search(term)
+        : Observable.of<Hero[]>([]))
+      .catch(error => {
+        console.log(error);
+        return Observable.of<Hero[]>([]);
+      });
+  }
+
+  search(term:string) {
+    this.searchTerms.next(term);
+  }
+
+  gotToDetail(hero:Hero) {
+    let link = ['/detail', hero.id];
+    this.router.navigate(link);
+  }
 }
